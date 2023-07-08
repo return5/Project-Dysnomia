4a6e6aa1c620844b8c5b402cee4bdc4e03f24c06
chris <armoredmoose@gmail.com>
2023-07-07 21:19:26 -0500

    added update ops

--- parser/Parser.lua
+++ parser/Parser.lua
@@ -2,7 +2,6 @@ local Scope <const> = require('parser.Scope')
 local setmetatable <const> = setmetatable
 local match <const> = string.match
 local Variable <const> = require('parser.Variable')
-local io = io
 
 local Parser <const> = {}
 Parser.__index = Parser
@@ -23,23 +22,21 @@ local function matchOrEmptyString(text,pat)
 	return match(text,pat) or #text == 0
 end
 
-function Parser:addOp(i)
+local keywords <const> = {
+	['then'] = true,
+	['do'] = true,
+	['end'] = true,
+	['function'] = true,
+	['if'] = true,
+	['elseif'] = true,
+	['else'] = true,
+	['while'] = true,
+	['for'] = true,
+	['local'] = true
+}
 
-	return i + 1
-end
-
-function Parser:subOp(i)
-
-	return i + 1
-end
-
-function Parser:divOp(i)
-
-	return i + 1
-end
-
-function Parser:multOp(i)
-	return i + 1
+local function matchFuncOrKeyWord(text,char)
+	return matchFunc(text,char) or keywords[trimString(text)]
 end
 
 local function addToVarName()
@@ -52,8 +49,45 @@ local function addToVarName()
 	end
 end
 
+local function addToMathOps()
+	local mathOps <const> = {}
+	return mathOps,function(text)
+		local str <const> = trimString(text)
+		if str and #str > 0 then
+			mathOps[#mathOps + 1] = str
+		end
+	end
+end
+
+function Parser:addOp(i)
+	return self:updateOps(i," +")
+end
+
+function Parser:subOp(i)
+	return self:updateOps(i," -")
+end
+
+function Parser:divOp(i)
+	return self:updateOps(i," /")
+
+end
+
+function Parser:multOp(i)
+	return self:updateOps(i," *")
+end
+
+local function doNothing() end
+
+function Parser:updateOps(i,op)
+	local varI <const> = self:loopBack(i - 1,matchFunc,"%s",doNothing)
+	self:writeDysText("= ")
+	self:writeDysText(self.text[varI])
+	self:writeDysText(op)
+	return i + 1
+end
+
 local function addToFlags()
-	local flags <const> = {['local'] = true}
+	local flags <const> = {['local'] = true,['const'] = true}
 	return flags,function(text)
 		local str <const> = trimString(text)
 		if str and #str > 0 and str ~= "," then
@@ -68,8 +102,9 @@ end
 
 function Parser:scrapeVarFlags(i)
 	local flagNames <const> ,flagFunc <const> = addToFlags()
-	local newI <const> = self:loopUntil(i + 1,match,"[^>]",flagFunc)
+	local newI <const> = self:loopUntil(i + 1,matchFunc,"[^>]",flagFunc)
 	if flagNames['global'] then flagNames['local'] = false end
+	if flagNames['immutable'] then flagNames['const'] = false end
 	return flagNames,newI + 1
 end
 
@@ -88,7 +123,7 @@ end
 
 function Parser:variable(i)
 	local varNames <const>, varNameFunc <const> = addToVarName()
-	local newI <const> = self:loopUntil(i + 1,match,"[^<=;\n]",varNameFunc)
+	local newI <const> = self:loopUntil(i + 1,matchFunc,"[^<=;\n]",varNameFunc)
 	local finalI = newI
 	if self.text[newI] == "<" then
 		local flags
@@ -100,11 +135,13 @@ function Parser:variable(i)
 	return finalI
 end
 
-function Parser:loopBack(from,toFunc,to)
+function Parser:loopBack(from,toFunc,to,doFunc)
 	local i = from
 	while toFunc(self.text[i],to) and i > 0 do
+		doFunc(self.text[i])
 		i = i - 1
 	end
+	return i
 end
 
 function Parser:loopUntil(from,toFunc,to,doFunc)
